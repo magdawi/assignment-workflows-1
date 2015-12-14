24e9e53db067adac519f39b4a72c70caefa46641
magdawi <mwimmer.mmt-b2014@fh-salzburg.ac.at>
2015-12-15 00:50:54 +0100

    implemented html_statement

--- customer.rb
+++ customer.rb
@@ -11,18 +11,28 @@ class Customer
   end
 
   def statement
-    frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      frequent_renter_points += element.frequent_renter_points
-      
       # show figures for this rental
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
-    
     # add footer lines
     result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{total_frequent_renter_points} frequent renter points"
+    result += "You earned #{ total_frequent_renter_points } frequent renter points"
+    result
+  end
+  
+  def html_statement
+    result = "<h1>Rentals for <em>#{@name}</em></h1><p>\n"
+    @rentals.each do |element|
+      # show figures for this rental
+      result += "\t" + element.movie.title + ": " + element.charge.to_s +   "<br>\n"
+    end
+    # add footer lines
+    result += "<p>You owe <em>#{total_charge}</em><p>\n"
+    result += "On this rental you earned " +
+         "<em>#{total_frequent_renter_points}</em> " +
+         "frequent renter points<p>"
     result
   end
 
