39c0b0e3ebb115c66ca0ab2ac55d2fa5b159537f
magdawi <mwimmer.mmt-b2014@fh-salzburg.ac.at>
2015-12-15 01:40:24 +0100

    insert delegator into frequent_renter_points, inserted uniformed tabs to files

--- movie.rb
+++ movie.rb
@@ -1,48 +1,63 @@
 class Movie
-  REGULAR = 0
-  NEW_RELEASE = 1
-  CHILDRENS = 2
+	REGULAR = 0
+	NEW_RELEASE = 1
+	CHILDRENS = 2
 
-  attr_reader :title
-  attr_reader :price_code
+	attr_reader :title
+	attr_reader :price_code
 
-  def initialize(title, the_price_code)
-    @title, self.price_code = title, the_price_code
-  end
+	def initialize(title, the_price_code)
+	  	@title, self.price_code = title, the_price_code
+	end
 
-  def price_code=(value)
-  	@price_code = value
-  	@price = case price_code
-  	  when REGULAR then RegularPrice.new
-  	  when NEW_RELEASE then NewReleasePrice.new
-  	  when CHILDRENS then ChildrensPrice.new
-  	end
-  end
+	def price_code=(value)
+		@price_code = value
+		@price = case price_code
+			when REGULAR then RegularPrice.new
+			when NEW_RELEASE then NewReleasePrice.new
+			when CHILDRENS then ChildrensPrice.new
+		end
+	end
 
-  def charge(days_rented)
-  	 @price.charge(days_rented)
-  end
+	def charge(days_rented)
+		@price.charge(days_rented)
+	end
 
-  def frequent_renter_points(days_rented)
-  	(price_code == NEW_RELEASE && days_rented > 1) ? 2 : 1
-  end
+	def frequent_renter_points(days_rented)
+		@price.frequent_renter_points(days_rented)
+	end
 end
 
+module DefaultPrice
+	def frequent_renter_points(days_rented)
+		1
+	end
+end
+
+
 class RegularPrice
+  	include DefaultPrice
+
   	def charge(days_rented)
-      result = 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-      result
+    	result = 2
+      	result += (days_rented - 2) * 1.5 if days_rented > 2
+      	result
     end
 end
 
 class NewReleasePrice
+	def frequent_renter_points(days_rented)
+		days_rented > 1 ? 2 : 1
+	end
+
 	def charge(days_rented)
-      days_rented * 3
+      	days_rented * 3
     end
 end
 
 class ChildrensPrice
+	include DefaultPrice
+
 	def charge(days_rented)
       result = 1.5
       result += (days_rented - 3) * 1.5 if days_rented > 3
